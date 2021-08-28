9cd374cf9168752a460dbf0f494790b5d3f3557e
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2021-08-28 07:41:23 +0430

    Update BlogServiceProvider.php

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -26,9 +26,14 @@ class BlogServiceProvider extends ServiceProvider
 
             $this->publishes([
                 __DIR__.'/../database/migrations/' => database_path('migrations'),
-                __DIR__.'/../database/factories/' => database_path('factories'),
-                __DIR__.'/../database/seeders/' => database_path('seeders'),
             ], 'blog-migrations');
+
+            $this->publishes([
+                __DIR__.'/../database/factories/' => database_path('factories'),
+            ], 'blog-factories');
+            $this->publishes([
+                __DIR__.'/../database/seeders/' => database_path('seeders'),
+            ], 'blog-seeders');
 
 
             // Publishing the views.
